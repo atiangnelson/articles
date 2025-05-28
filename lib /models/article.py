f363ea237baab5acd8acca56b9d397bcc51dc857
Nelson <nelson.atiang@student.moringaschool.com>
2025-05-29 02:37:11 +0300

    validated the title of the article

--- lib /models/article.py
+++ lib /models/article.py
@@ -6,3 +6,13 @@ class Article:
         self.title = title  
         self.author_id = author_id
         self.magazine_id = magazine_id
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if not isinstance(value, str) or value == "":
+            raise ValueError("Article title must be a non-empty string.")
+        self._title = value    
