f010a6daa8d74ee51b2c69b5468bfbdb3e462f13
Nelson <nelson.atiang@student.moringaschool.com>
2025-05-29 02:27:44 +0300

    icluded author and articles method to work with relationtips

--- lib /models/magazine.py
+++ lib /models/magazine.py
@@ -28,20 +28,73 @@ class Magazine:
         self._category = value
     
 
-def save(self):
+    def save(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        if self.id is None:
+            cursor.execute(
+                "INSERT INTO magazines (name, category) VALUES (?, ?)",
+                (self.name, self.category)
+            )
+            self.id = cursor.lastrowid
+        else:
+            cursor.execute(
+                "UPDATE magazines SET name = ?, category = ? WHERE id = ?",
+                (self.name, self.category, self.id)
+            )
+        conn.commit()
+        conn.close()
+
+@classmethod
+def find_by_id(cls, magazine_id):
     conn = get_connection()
     cursor = conn.cursor()
-
-    if self.id is None:
-        cursor.execute(
-            "INSERT INTO magazines (name, category) VALUES (?, ?)",
-            (self.name, self.category)
-        )
-        self.id = cursor.lastrowid
-    else:
-        cursor.execute(
-            "UPDATE magazines SET name = ?, category = ? WHERE id = ?",
-            (self.name, self.category, self.id)
-        )
-    conn.commit()
+    cursor.execute("SELECT * FROM magazines WHERE id = ?", (magazine_id,))
+    row = cursor.fetchone()
     conn.close()
+    if row:
+        return cls(row["name"], row["category"], row["id"])
+    return None
+
+@classmethod
+def find_by_name(cls, name):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM magazines WHERE name = ?", (name,))
+    row = cursor.fetchone()
+    conn.close()
+    if row:
+        return cls(row["name"], row["category"], row["id"])
+    return None
+
+@classmethod
+def find_by_category(cls, category):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM magazines WHERE category = ?", (category,))
+    rows = cursor.fetchall()
+    conn.close()
+    return [cls(row["name"], row["category"], row["id"]) for row in rows]
+
+def articles(self):
+
+    from lib.models.article import Article
+
+    return Article.find_by_magazine(self.id)
+
+def authors(self):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT DISTINCT authors.id, authors.name
+        FROM authors
+        JOIN articles ON authors.id = articles.author_id
+        WHERE articles.magazine_id = ?
+    """, (self.id,))
+    rows = cursor.fetchall()
+    conn.close()
+
+    from lib.models.author import Author
+    
+    return [Author(row["name"], row["id"]) for row in rows]
