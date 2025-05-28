5806388ec2eb92195b11f5b0c5ec0ec31d9b45fc
Nelson <nelson.atiang@student.moringaschool.com>
2025-05-29 02:38:36 +0300

    created a query that saves the magazine to the database

--- lib /models/article.py
+++ lib /models/article.py
@@ -15,4 +15,24 @@ class Article:
     def title(self, value):
         if not isinstance(value, str) or value == "":
             raise ValueError("Article title must be a non-empty string.")
-        self._title = value    
+        self._title = value  
+
+
+    def save(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        if self.id is None:
+            cursor.execute(
+                "INSERT INTO articles (title, author_id, magazine_id) VALUES (?, ?, ?)",
+                (self.title, self.author_id, self.magazine_id)
+            )
+            self.id = cursor.lastrowid
+        else:
+            cursor.execute(
+                "UPDATE articles SET title = ?, author_id = ?, magazine_id = ? WHERE id = ?",
+                (self.title, self.author_id, self.magazine_id, self.id)
+            )
+
+        conn.commit()
+        conn.close()      
